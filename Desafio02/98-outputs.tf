7cb63ebfa2113662b691e0984cca36e6449f50f7
Diogo Fernandes <diogo.souza@fortbrasil.com.br>
2022-03-07 23:11:12 -0300

    Commit in [07-03-2022 23:11:12]

--- Desafio02/98-outputs.tf
+++ Desafio02/98-outputs.tf
@@ -1,3 +1,7 @@
 output "primary_domain" {
-  value = data.azuread_domains.primary_domain
+  value = data.azuread_domains.primary_domain.domains.0.domain_name
 }
+output "secundary_domain" {
+  value = data.azuread_domains.primary_domain.domains.3.domain_name
+}
+
