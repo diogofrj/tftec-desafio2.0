166bd55b3504528d0f8a396017f32d9e5cc2422a
Diogo Fernandes <diogo.souza@fortbrasil.com.br>
2022-03-08 00:02:28 -0300

    Commit in [08-03-2022 00:02:28]

--- Desafio02/aad.tf
+++ Desafio02/aad.tf
@@ -3,4 +3,6 @@ resource "azuread_user" "svc_azure" {
   user_principal_name = "svc.azure@${data.azuread_domains.primary_domain.domains.2.domain_name}"
   display_name        = "Service Account AD Connect"
   password            = "Partiunuvem@123"
+  country = "BR"
+  
 }
